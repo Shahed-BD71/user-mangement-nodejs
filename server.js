c187dd04fa9f05b087962f4d3a9dfa961291a29a
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-09-06 16:40:30 +0600

    displaying-random-user

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
-let ejs = require('ejs');
-const data = require('./data.json')
+const bodyParser = require("body-parser");
+let ejs = require("ejs");
+const data = require("./data.json");
 // const dbConnect = require('./utils/dbConnect');
 const userRoutes = require("./routes/v1/users.route.js");
 
-
 // middleware
 app.use(express.json());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // app.use(express.static("public"))
-app.use(express.static("views"))
-app.set("view engine", ejs)
-
+app.use(express.static("views"));
+app.set("view engine", ejs);
 
 // Routes
-app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/users", userRoutes);
 app.get("/", (req, res) => {
-  // serve raw html file
-  // res.sendFile(__dirname + "/public/index.html")
-  // res.sendFile("index.html")
-  // serve dynamic fie by ejs
-
+  const userData = data.sort(() => 0.5 - Math.random()).slice(0, 10)
   res.render("home.ejs", {
-     allUser: data
-  })
+    allUser: userData,
+  });
 });
 
 app.all("*", (req, res, next) => {
